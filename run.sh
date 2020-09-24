436ec076618a143dd0241cf640e4181f3c263c22
daneshg <daneshdani@gmail.com>
2020-09-24 17:08:44 +0530

    Removed default values of apache and mysql images

--- run.sh
+++ run.sh
@@ -6,8 +6,14 @@ export SITE_NAME=${SITE_NAME:-"My MediaWiki"}
 export NODE_PORT=${NODE_PORT:-30003}
 export SITE_URL="http://$(minikube ip | sed 's/\"//g'):${NODE_PORT}"
 
-export APACHE_IMAGE_FULL_PATH=${APACHE_IMAGE_FULL_PATH:-my_repo:5000/mediawiki-apache2-php:1.0}
-export MYSQL_IMAGE_FULL_PATH=${MYSQL_IMAGE_FULL_PATH:-my_repo:5000/mediawiki-mysql:1.0}
+export APACHE_IMAGE_FULL_PATH=${APACHE_IMAGE_FULL_PATH}
+export MYSQL_IMAGE_FULL_PATH=${MYSQL_IMAGE_FULL_PATH}
+
+if [[ -z $APACHE_IMAGE_FULL_PATH || -z $MYSQL_IMAGE_FULL_PATH  ]];
+then
+  echo -e "Please update image names ..."
+  exit 2
+fi 
 
 SCRIPTS_DIR="$(pwd)/deployments"
 
